04250377f844a5ee0f7c8bbbbd3ebb1f1be7b462
Leo6485 <lo570354@gmail.com>
2025-02-11 00:29:12 -0300

    Refactor JSONBin module to support multiple BIN IDs and improve IP retrieval logic

--- modules/jsonbin.py
+++ modules/jsonbin.py
@@ -1,29 +1,49 @@
 import requests
+from time import localtime
 
-BIN_ID = "676154dead19ca34f8dc8c9e"
-API_URL = f"https://api.jsonbin.io/v3/b/{BIN_ID}"
+minutes = localtime().tm_min
+
+BIN_IDS = ["67aa72d0e41b4d34e488e836", "67aa7636ad19ca34f8fea68e"]
+API_URLS = [f"https://api.jsonbin.io/v3/b/{bin_id}" for bin_id in BIN_IDS]
+
+HEADERS = {
+    "Content-Type": "application/json",
+}
 
 def set_ip(ip):
-    headers = {
-        "Content-Type": "application/json",
-    }
     data = {"server_ip": ip}
     
-    try:
-        response = requests.put(API_URL, json=data, headers=headers)
-        if response.status_code != 200:
-            print("[ ! ] Erro ao atualizar IP:", response.text)
-            return 0
+    for url in API_URLS:
 
-        return 1
-    except:
-        return 0
+        try:
+            response = requests.put(url, json=data, headers=HEADERS)
+
+            if response.status_code == 200:
+                return 1
+            else:
+                print("[ ! ] Erro ao atualizar IP:", response.text)
+
+        except Exception as e:
+            print(f"Erro ao atualizar IP: {e}")
+    return 0
 
 def get_ip():
-    try:
-        assert False
-        response = requests.get(API_URL + "/latest").json()
-        return response["record"]["server_ip"] or "Erro"
-    except Exception as e:
-        print(f"Erro ao obter endereço ip: {e}")
-        return input("Digite o ip manualmente: ")
+    for url in API_URLS:
+
+        try:
+            response = requests.get(url + "/latest").json()
+            print(response)
+            ip = response["record"]["server_ip"]
+            dt = abs(int(response["metadata"]["createdAt"][14:16]) - minutes)
+            
+            print(dt)
+            if 10 > dt > 50:
+                print(f"Endereço desatualizado: {ip}")
+                continue
+
+            return ip
+
+        except Exception as e:
+            print(f"Erro ao obter endereço ip: {e}")
+
+    return input("Digite o ip manualmente: ")
